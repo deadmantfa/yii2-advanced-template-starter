59643e2c9eda8ee4e22d18cfcedccd3e7aeafbeb
deadmantfa <wenceslausdsilva@gmail.com>
2020-04-10 23:53:29 +0530

    Updated provisio scripts

--- vagrant/provision/once-as-root.sh
+++ vagrant/provision/once-as-root.sh
@@ -75,6 +75,31 @@ echo "Done!"
 info "Install additional software"
 apt-get install -y gcc g++ make php7.2-curl php7.2-cli php7.2-intl php7.2-mysqlnd php7.2-gd php7.2-fpm php7.2-mbstring php7.2-xml unzip nginx mysql-server-5.7 php.xdebug php7.2-dev php7.2-bcmath
 
+
+info "Update OS software"
+rm -Rf /etc/apt/sources.list.d/elastic-5.x.list
+wget -q -O - https://artifacts.elastic.co/GPG-KEY-elasticsearch | apt-key add -
+rm -Rf /etc/apt/sources.list.d/elastic-5.x.list
+echo "deb https://artifacts.elastic.co/packages/5.x/apt stable main" | tee -a /etc/apt/sources.list.d/elastic-5.x.list
+apt-get update
+apt-get upgrade -y
+apt-get install -y curl apache2-utils apt-transport-https openjdk-8-jre jq
+
+info "Install ElasticSearch"
+apt-get install -y elasticsearch
+systemctl start elasticsearch
+systemctl enable elasticsearch
+sleep 20
+curl -X GET "localhost:9200" | jq '.'
+echo "Done!"
+
+info "Install Kibana"
+apt-get install -y kibana
+systemctl enable kibana
+systemctl start kibana
+echo "Done!"
+
+
 info "Install Node 12 LTS"
 curl -sL https://deb.nodesource.com/setup_12.x | sudo -E bash -
 apt-get install -y nodejs
